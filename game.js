4553e06ab2463bbf47680db47da753a121786b92
bvreeman <bvreeman@gmail.com>
2018-04-26 18:26:42 -0500

    added another code in order to understand what is going on.

---
+++ game.js
@@ -0,0 +1,4 @@
+// list of words to guess.
+exports.newWord = {
+  wordList: ['SUPERMAN', 'BATMAN', 'WONDER WOMAN', 'THE FLASH', 'AQUAMAN', 'CYBORG', 'BATGIRL', 'ROBIN', 'NIGHTWING', 'BLACK CANARY', 'HAWKGIRL', 'GREEN ARROW', 'SUPERGIRL'],
+};
--- word.js
+++ word.js
@@ -1,19 +1,55 @@
+// require letter objects
 const Letter = require('./letter.js');
 
-class Word {
-    constructor(Letter) {
-      // A string value to store the underlying character for the letter
-      this.Letter = [];
-      // A boolean value that stores whether that letter has been guessed yet
-      this.guessed = true;
-    }
-    // A function that returns the underlying character if the letter has been guessed,
-    // or a placeholder (like an underscore) if the letter has not been guessed
-    letterGuessed() {
-        if () {
-            
-        }
-    }
-  }
+function Word(wrd) {
+  const that = this;
+  // store the string wrd
+  this.word = wrd;
+  // collection of letter objects
+  this.letters = [];
+  this.wordFound = false;
 
-  module.exports = Word;
+  this.getLets = function() {
+    // populate the collection above with new Letter objects
+    for (let i = 0; i < that.word.length; i++) {
+      const newLetter = new Letter(that.word[i]);
+      this.letters.push(newLetter);
+    }
+  };
+
+  // found the current word
+  this.didWeFindTheWord = function() {
+    if (this.letters.every(function(lttr) {
+      return lttr.appear === true;
+    })) {
+      this.wordFound = true;
+      return true;
+    }
+  };
+
+  this.checkIfLetterFound = function(guessedLetter) {
+    let whatToReturn = 0;
+    // iterates through each letter to see if it matches the guessed letter
+    this.letters.forEach(function(lttr) {
+      if (lttr.letter === guessedLetter) {
+        lttr.appear = true;
+        whatToReturn++;
+      }
+    });
+    // if guessLetter matches Letter property, the letter object should be shown
+    return whatToReturn;
+  };
+
+  this.wordRender = function() {
+    let display = '';
+    // render the word based on if letters are found or not
+    that.letters.forEach(function(lttr) {
+      const currentLetter = lttr.letterRender();
+      display += currentLetter;
+    });
+
+    return display;
+  };
+}
+
+module.exports = Word;
